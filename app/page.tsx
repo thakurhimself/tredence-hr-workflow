f8e991dbf9905954a960413296f6e7a2e24669ea
Vipul Thakur <vipulthakurofficial@gmail.com>
2025-12-07 04:11:46 +0530

    cleanup

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,10 @@
 import RootComponent from "@/components/RootComponent";
-import { RootContextProvider } from "@/context/RootContext";
 import { WorkflowProvider } from "@/context/WorkflowContext";
 
 export default function Home() {
   return (
     <WorkflowProvider>
-      <RootContextProvider>
         <RootComponent />
-      </RootContextProvider>
     </WorkflowProvider>
     
   )
